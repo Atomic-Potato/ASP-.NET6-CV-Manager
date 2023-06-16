6644a41fef8cabe38d21e0601b0c3111f4eb96ed
Atomic Potato <medyanmehiddine@gmail.com>
2023-06-16 20:38:45 +0300

    Layout - added the main razor pages

---
+++ CV Manager/Pages/Browse.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV_Manager.Pages
+{
+    public class BrowseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
