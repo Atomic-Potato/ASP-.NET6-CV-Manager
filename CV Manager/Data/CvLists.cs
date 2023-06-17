adf2ce60ca74720dbaa4bdf8f0b95637e5cd5db8
Atomic Potato <medyanmehiddine@gmail.com>
2023-06-17 19:04:35 +0300

    Send - Static CV lists

---
+++ CV Manager/Data/CvLists.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CV_Manager.Data {
+    public class CvLists {
+
+        public static IEnumerable<SelectListItem> nationalities = new List<SelectListItem>() {
+            new SelectListItem{Value = "LB", Text = "Lebanese"},
+            new SelectListItem{Value = "JP", Text = "Japanese"},
+            new SelectListItem{Value = "FR", Text = "French"},
+            new SelectListItem{Value = "O", Text = "Other"}
+        };
+
+        public static IEnumerable<SelectListItem> genders = new List<SelectListItem>() {
+            new SelectListItem{Value = "M", Text = "Male"},
+            new SelectListItem{Value = "F", Text = "Female"},
+            new SelectListItem{Value = "AH", Text = "Attack Helicopter"},
+            new SelectListItem{Value = "O", Text = "Other"}
+        };
+
+        public static IEnumerable<SelectListItem> skills = new List<SelectListItem>() {
+            new SelectListItem{Value = "cs", Text = "C#"},
+            new SelectListItem{Value = "java", Text = "Java"},
+            new SelectListItem{Value = "py", Text = "Python"},
+            new SelectListItem{Value = "beef", Text = "Beef"}
+        };
+    }
+}
--- CV Manager/Pages/Send.cshtml.cs
+++ CV Manager/Pages/Send.cshtml.cs
@@ -10,27 +10,6 @@ namespace CV_Manager.Pages
         [BindProperty]
         public BaseCV cv {get; set;}
         
-        public IEnumerable<SelectListItem> nationalities = new List<SelectListItem>() {
-            new SelectListItem{Value = "LB", Text = "Lebanese"},
-            new SelectListItem{Value = "JP", Text = "Japanese"},
-            new SelectListItem{Value = "FR", Text = "French"},
-            new SelectListItem{Value = "O", Text = "Other"}
-        };
-
-        public IEnumerable<SelectListItem> genders = new List<SelectListItem>() {
-            new SelectListItem{Value = "M", Text = "Male"},
-            new SelectListItem{Value = "F", Text = "Female"},
-            new SelectListItem{Value = "AH", Text = "Attack Helicopter"},
-            new SelectListItem{Value = "O", Text = "Other"}
-        };
-
-        public IEnumerable<SelectListItem> skills = new List<SelectListItem>() {
-            new SelectListItem{Value = "cs", Text = "C#"},
-            new SelectListItem{Value = "java", Text = "Java"},
-            new SelectListItem{Value = "py", Text = "Python"},
-            new SelectListItem{Value = "beef", Text = "Beef"}
-        };
-
         public void OnGet(){
             cv = new BaseCV();
         }
