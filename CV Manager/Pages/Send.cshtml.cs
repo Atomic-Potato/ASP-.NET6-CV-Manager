11541b5f4b47da77b982a9b473389799c8aa3ce4
Atomic Potato <medyanmehiddine@gmail.com>
2023-06-17 19:10:08 +0300

    Send - Basic page redirection

--- CV Manager/Pages/Send.cshtml.cs
+++ CV Manager/Pages/Send.cshtml.cs
@@ -13,5 +13,10 @@ namespace CV_Manager.Pages
         public void OnGet(){
             cv = new BaseCV();
         }
+
+        // TODO: Convert to async Task once database stuff is implemented
+        public IActionResult OnPost() {
+            return RedirectToPage("Summary", new { id = "Insert_ID" });
+        }
     }
 }
