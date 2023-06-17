f21f4738eefdf66eec79b559c3569e95d06eb862
Atomic Potato <medyanmehiddine@gmail.com>
2023-06-17 14:01:05 +0300

    Send - CV table class

---
+++ CV Manager/Data/CV.cs
@@ -0,0 +1,25 @@
+﻿namespace CV_Manager.Data {
+    public class CV {
+        public int id;
+        public string firstName;
+        public string lastName;
+        public DateTime birthDay;
+
+        // Dropdown List
+        public string nationality;
+           
+        // Radio Button
+        public string gender;
+
+        #region PROGRAMMING SKILLS [CheckBoxes]
+        public bool java;
+        public bool cs;
+        public bool python;
+        #endregion
+
+        public EmailAddressAttribute emailAddress;
+        public EmailAddressAttribute confirmEmailAddress;
+
+        public File photo;
+    }
+}
