444eef06f71ea12b5a425a9df0329178113efe25
Atomic Potato <medyanmehiddine@gmail.com>
2023-06-18 19:33:41 +0300

    Summary - Details for each entry

--- CV Manager/Pages/Browse.cshtml.cs
+++ CV Manager/Pages/Browse.cshtml.cs
@@ -5,8 +5,28 @@ namespace CV_Manager.Pages
 {
     public class BrowseModel : PageModel
     {
-        public void OnGet()
-        {
+        [BindProperty]
+        public IEnumerable<CV> cvs { get; set; }
+
+        CVService service;
+
+        public BrowseModel(CVService service) { 
+            this.service = service;
+        }
+
+        public async Task OnGet(){
+            cvs = await service.LoadAllCVs();
+        }
+
+        public IActionResult OnPost(string form, int cvId) {
+            if (form.StartsWith("details-"))
+                return RedirectToPage("Summary", new { id = cvId });
+            if (form.StartsWith("edit-"))
+                return RedirectToPage("Edit", new { id = cvId });
+            if (form.StartsWith("delete-"))
+                return RedirectToPage("Delete", new { id = cvId });
+
+            return Page();
         }
     }
 }
