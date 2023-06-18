8a73a9354c1bdaddb042c7c629605897e5269e45
Atomic Potato <medyanmehiddine@gmail.com>
2023-06-18 14:24:47 +0300

    Summary - Displayed data

--- CV Manager/Data/CvLists.cs
+++ CV Manager/Data/CvLists.cs
@@ -4,17 +4,17 @@ namespace CV_Manager.Data {
     public class CVLists {
 
         public static IEnumerable<SelectListItem> nationalities = new List<SelectListItem>() {
-            new SelectListItem{Value = "LB", Text = "Lebanese"},
-            new SelectListItem{Value = "JP", Text = "Japanese"},
-            new SelectListItem{Value = "FR", Text = "French"},
-            new SelectListItem{Value = "O", Text = "Other"}
+            new SelectListItem{Value = "Lebanon", Text = "Lebanese"},
+            new SelectListItem{Value = "Japan", Text = "Japanese"},
+            new SelectListItem{Value = "France", Text = "French"},
+            new SelectListItem{Value = "unkown country", Text = "Other"}
         };
 
         public static IEnumerable<SelectListItem> genders = new List<SelectListItem>() {
-            new SelectListItem{Value = "M", Text = "Male"},
-            new SelectListItem{Value = "F", Text = "Female"},
-            new SelectListItem{Value = "AH", Text = "Attack Helicopter"},
-            new SelectListItem{Value = "O", Text = "Other"}
+            new SelectListItem{Value = "Male", Text = "Male"},
+            new SelectListItem{Value = "Female", Text = "Female"},
+            new SelectListItem{Value = "Attack Helicopter", Text = "Attack Helicopter"},
+            new SelectListItem{Value = "unkown gender", Text = "Other"}
         };
 
         public static IEnumerable<SelectListItem> skills = new List<SelectListItem>() {
